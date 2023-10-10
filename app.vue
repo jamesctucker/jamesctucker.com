83eb7401e7446e65ee831d55be6f6a44c7e513ed
James Tucker <jamesctucker@protonmail.com>
2023-10-09 23:43:49 -0500

    more goofing around, add headshot

--- app.vue
+++ app.vue
@@ -18,19 +18,49 @@ import "./styles/app.scss";
           <h1><bold>JAMES</bold></h1>
         </template>
       </BentoCard>
-      <BentoCard :background-color="Charcoal">
+      <BentoCard :background-color="Charcoal" padding="0">
         <template #content>
-          <h2>IS</h2>
+          <img src="./assets/images/headshot.jpg" alt="James" />
         </template>
       </BentoCard>
       <BentoCard :background-color="Charcoal">
         <template #content>
-          <h2>A</h2>
+          <h2>IS A</h2>
         </template>
       </BentoCard>
       <BentoCard :background-color="Charcoal">
         <template #content>
-          <h2>SOFTWARE ENGINEER.</h2>
+          <h2>SOFTWARE ENGINEER</h2>
+        </template>
+      </BentoCard>
+      <BentoCard :background-color="Charcoal">
+        <template #content>
+          <h2>IN</h2>
+        </template>
+      </BentoCard>
+      <BentoCard :background-color="GoldenHour">
+        <template #content>
+          <h2 class="location">MINNESOTA</h2>
+        </template>
+      </BentoCard>
+      <BentoCard :background-color="Charcoal">
+        <template #content>
+          <h2>"To be or not to be, that is the question."</h2>
+        </template>
+      </BentoCard>
+      <BentoCard :background-color="Charcoal">
+        <template #content>
+          <h2>I occasionally write words.</h2>
+        </template>
+      </BentoCard>
+      <BentoCard :background-color="Charcoal">
+        <template #content>
+          <h2>I like to take photos.</h2>
+        </template>
+      </BentoCard>
+      <BentoCard :background-color="GoldenHour">
+        <template #content>
+          <h2 class="contact">SAY HELLO</h2>
         </template>
       </BentoCard>
     </BentoGrid>
@@ -51,5 +81,17 @@ h2 {
   font-family: "Josefin Sans", sans-serif;
   font-weight: 400;
   font-size: 3rem;
+}
+
+// TODO: stop hardcoding this class
+.location,
+.contact {
+  color: v-bind("Gunmetal");
+}
+
+img {
+  width: 100%;
+  min-width: 300px;
+  border-radius: 8px;
 }
 </style>
--- components/BentoCard.vue
+++ components/BentoCard.vue
@@ -4,6 +4,10 @@ const props = defineProps({
     type: String,
     default: "white",
   },
+  padding: {
+    type: String,
+    default: "16px",
+  },
 });
 </script>
 
@@ -13,16 +17,41 @@ const props = defineProps({
   </div>
 </template>
 <style lang="scss" scoped>
+@use "./styles/_variables_colors.scss" as colors;
 .bento-card {
   --background-color: v-bind("backgroundColor");
+  --padding: v-bind("padding");
 
   background-color: var(--background-color);
   border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  padding: 16px;
+  padding: var(--padding);
   display: flex;
   align-items: center;
   justify-content: center;
   flex: 1;
 }
+
+// on hover, mimic pushing a key down on a typewriter
+.bento-card:hover {
+  animation: keypress 0.2s linear;
+  cursor: pointer;
+}
+
+// remove box shadow gradually to mimic key being released
+@keyframes keypress {
+  0% {
+    transform: translateY(0px);
+  }
+  50% {
+    transform: translateY(4px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+}
+
+// TODO: add hover style that has a custom cursor (I want to add a paper airplane icon)
+// .bento-card:hover {
+//   cursor: pointer;
+// }
 </style>
